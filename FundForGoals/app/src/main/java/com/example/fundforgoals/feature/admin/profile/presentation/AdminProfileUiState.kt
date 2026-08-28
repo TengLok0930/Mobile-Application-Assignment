2ef20ab351a2c43d8b4c398880320ea4643ee230
kleppy <stephenchoiweikein@gmail.com>
2026-08-28 14:37:47 +0800

    Files to delete

--- FundForGoals/app/src/main/java/com/example/fundforgoals/feature/admin/profile/presentation/AdminProfileUiState.kt
+++
@@ -1,9 +0,0 @@
-package com.example.fundforgoals.feature.admin.profile.presentation
-
-data class AdminProfileUiState(
-    val isLoading: Boolean = false,
-    val adminName: String = "Administrator",
-    val appearanceLabel: String = "Dark",
-    val notificationsLabel: String = "On",
-    val errorMessage: String? = null
-)
